78d43e8b27f282e9f1cfdd4c7629ed24c2f6dbfd
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-08-23 13:34:52 +0530

    Initial Commit

---
+++ ThreadClone/ThreadCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadCloneApp.swift
+//  ThreadClone
+//
+//  Created by Yashwant Sheshkar on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
